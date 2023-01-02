0095a084ace40727bbb513d73db9d4d122d4ae1a
krzk426 <krzk426@gmail.com>
2023-01-02 22:07:42 +0100

    change: read .caddo file

--- caddo_file_parser/caddo_file_parser.py
+++ caddo_file_parser/caddo_file_parser.py
@@ -16,7 +16,7 @@ class CaddoFileParser:
     def save_data(self, caddo_file):
         pd.DataFrame(caddo_file.data).to_csv(
             "data.csv",
-            sep=caddo_file.separator,
+            sep=';',
             index=False
         )
 
@@ -30,12 +30,12 @@ class CaddoFileParser:
                 "train_indexes": train_indexes,
                 "test_indexes": test_indexes
             }
-            with open(f"fold_{fold_number}", 'w') as file:
-                yaml.dump(file_content, file)
+            with open(f"fold_{fold_number}.yaml", 'w') as file:
+                yaml.dump(file_content, file, Dumper=Dumper, default_flow_style=False)
 
     def pack_to_caddo_file(self, caddo_file, file_name):
-        filenames = [f"fold_{fold.number}.yaml" for fold in caddo_file.folds] + ["data.csv"]
-        with zipfile.ZipFile(f"{file_name}.caddo", mode="w") as archive:
+        filenames = [f"fold_{fold.number}.yaml" for fold in caddo_file.folds] + ["data.csv"] + ["settings.yaml"]
+        with zipfile.ZipFile(f"{file_name}.caddo", "w") as archive:
             for filename in filenames:
                 archive.write(filename)
 
@@ -45,6 +45,28 @@ class CaddoFileParser:
             os.remove(file)
 
     def read_data(self, file_name) -> CaddoFile:
-        
 
-        
+        with zipfile.ZipFile(file_name + ".caddo", "r") as zf:
+            settings = self.read_settings(zf)
+            data = self.read_csv_data(zf, settings)
+            folds = self.read_folds(zf, settings)
+        caddo_file: CaddoFile = CaddoFile(folds, data, settings)
+        return caddo_file
+
+    def read_settings(self, zf):
+        settings_file = zf.read("settings.yaml").decode(encoding="utf-8")
+        return yaml.load(settings_file, Loader=SafeLoader)
+
+    def read_csv_data(self, zf, settings):
+        separator = settings["output"]["separator"]
+        data_csv = zf.read("data.csv").decode(encoding="utf-8")
+        return pd.read_csv(io.StringIO(data_csv), sep=separator)
+
+    def read_folds(self, zf, settings):
+        folds = []
+        runs = settings["model"]["runs"]
+        for i in range(runs):
+            file = zf.read(f"fold_{i}.yaml").decode(encoding="utf-8")
+            data: Fold = yaml.load(file, Loader=SafeLoader)
+            folds.append(data)
+        return folds
