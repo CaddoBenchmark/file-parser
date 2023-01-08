9d2a1604902fef26d6aceea8444476b8ccc9fe64
krzk426 <krzk426@gmail.com>
2023-01-08 20:29:46 +0100

    merge

--- caddo_file_parser/settings/settings_loader.py
+++ caddo_file_parser/settings/settings_loader.py
@@ -3,7 +3,7 @@ from caddo_file_parser.settings.settings import Settings
 
 class SettingsLoader:
     def load_settings_object(self, settings_file):
-        settings_data: Settings = None
+        settings_data: Settings = Settings()
         settings_data.data_source_path = settings_file['data_source']['path']
         settings_data.data_source_separator = settings_file['data_source']['separator']
         settings_data.data_source_x_cols = settings_file['data_source']['x_columns_names']
