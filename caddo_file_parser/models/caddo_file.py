a982162bca616f274c24d13452271086c54d1105
ptylczynski <piotr@ptl.cloud>
2023-01-10 16:45:04 +0100

    change: rename fold into index set in caddo_file.py

--- caddo_file_parser/models/caddo_file.py
+++ caddo_file_parser/models/caddo_file.py
@@ -1,12 +1,12 @@
-from caddo_file_parser.models.fold import Fold
+from caddo_file_parser.models.index_set import IndexSet
 import pandas as pd
 
 from caddo_file_parser.settings.generation_settings import GenerationSettings
 
 
 class CaddoFile:
-    def __init__(self, folds: [Fold], data: pd.DataFrame, settings: GenerationSettings):
-        self.folds = folds
+    def __init__(self, index_sets: [IndexSet], data: pd.DataFrame, settings: GenerationSettings):
+        self.index_sets = index_sets
         self.data = data
         self.settings = settings
 
