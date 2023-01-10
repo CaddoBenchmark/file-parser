4d6ce21c32fda9c98e2caac36b9f638dd1843f9b
ptylczynski <piotr@ptl.cloud>
2023-01-10 17:12:38 +0100

    change: update cadd_file to use runs instead of index_sets

--- caddo_file_parser/models/caddo_file.py
+++ caddo_file_parser/models/caddo_file.py
@@ -1,12 +1,13 @@
 from caddo_file_parser.models.index_set import IndexSet
 import pandas as pd
 
+from caddo_file_parser.models.run import Run
 from caddo_file_parser.settings.generation_settings import GenerationSettings
 
 
 class CaddoFile:
-    def __init__(self, index_sets: [IndexSet], data: pd.DataFrame, settings: GenerationSettings):
-        self.index_sets = index_sets
+    def __init__(self, runs: [Run], data: pd.DataFrame, settings: GenerationSettings):
+        self.runs = runs
         self.data = data
         self.settings = settings
 
