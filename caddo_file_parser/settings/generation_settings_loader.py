719573758e44038cf09e51d023e017a815bf97e2
ptylczynski <piotr@ptl.cloud>
2023-01-10 17:39:46 +0100

    change: move runs number to data.splitting

--- caddo_file_parser/settings/generation_settings_loader.py
+++ caddo_file_parser/settings/generation_settings_loader.py
@@ -9,7 +9,7 @@ class GenerationSettingsLoader:
         settings_data.data_input_separator = settings_file["data"]['input']['separator']
         settings_data.data_extraction_function_path = settings_file["data"]['extraction']['function']['path']
         settings_data.data_splitting_folding_number = settings_file["data"]['splitting']['folding']['number']
-        settings_data.data_splitting_folding_runs = settings_file["data"]['splitting']['folding']['runs']
+        settings_data.data_splitting_runs = settings_file["data"]['splitting']['runs']
         settings_data.data_output_file_name = settings_file["data"]['output']['file']['name']
         settings_data.data_splitting_folding_method = settings_file["data"]['splitting']['folding']['method']
         settings_data.data_splitting_folding_seeds_from_list = settings_file["data"]['splitting']['folding']['seeds']['from_list']
