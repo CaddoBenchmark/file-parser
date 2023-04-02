59e9d9b7caadd825fa867b172f5617d27e32f7af
kkulesa <katarzyna.kulesa@openalytics.eu>
2023-04-02 12:35:35 +0200

    fix

--- caddo_file_parser/caddo_file_parser.py
+++ caddo_file_parser/caddo_file_parser.py
@@ -67,7 +67,7 @@ class CaddoFileParser:
     def pack_to_caddo_file(self, caddo_file):
         filenames = []
         for run in caddo_file.runs:
-            filenames += [f"index_set_{index_set.number}_run_{run.number}.yaml" for index_set in caddo_file.runs]
+            filenames += [f"index_set_{index_set.number}_run_{run.number}.yaml" for index_set in caddo_file.index_sets]
         filenames += ["data.csv"] + [caddo_file.settings.data_settings_file_path] + ["seeds.yaml"]
         with zipfile.ZipFile(f"{caddo_file.settings.data_output_file_name}.caddo", "w") as archive:
             for filename in filenames:
@@ -75,7 +75,7 @@ class CaddoFileParser:
 
     def remove_unused_file(self, caddo_file):
         for run in caddo_file.runs:
-            filenames = [f"index_set_{index_set.number}_run_{run.number}.yaml" for index_set in caddo_file.runs]
+            filenames = [f"index_set_{index_set.number}_run_{run.number}.yaml" for index_set in caddo_file.index_sets]
             for file in filenames:
                 os.remove(file)
         os.remove("data.csv")
